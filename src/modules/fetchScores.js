e66a681f52815c40890de7eee9007607507f1dc8
baroka <birotori@gmail.com>
2022-05-11 12:58:36 +0100

    Consume Leaderboard API.

---
+++ src/modules/fetchScores.js
@@ -0,0 +1,35 @@
+import Storage from './storage.js';
+
+export default class FetchScores {
+  constructor(){}
+
+  static getScore(url){
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        Storage.setItems(json.result)
+        const output = document.querySelector('.output');
+        output.innerHTML = '';
+        json.result.forEach((item) => {
+          const tr = document.createElement('tr');
+          tr.innerHTML = `${item.user} : ${item.score}`;
+          output.appendChild(tr);
+        });
+      });
+  }
+
+  static sendScore(url, user, score) {
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: user,
+        score: score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json.result));
+  }
+}
